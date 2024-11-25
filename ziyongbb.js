a07ff959d4429041813fedc8a83b4bcf4cc5b51e
Fly961219 <787954045@qq.com>
2024-11-25 18:02:21 +0800

    Update ziyongbb.js

--- ziyongbb.js
+++ ziyongbb.js
@@ -1,7 +1,7 @@
 [rewrite_local]
 # 自用插件
-^https?:\/\/app\.bilibili\.com\/x\/v2\/(account\/(myinfo|mine)|feed\/index)\? url script-request-header modify_bilibili_request.js
-^https?:\/\/app\.bilibili\.com\/bilibili\.app\.playerunite\.v1\.Player\/PlayViewUnite url script-request-header modify_bilibili_request.js
+^https?:\/\/app\.bilibili\.com\/x\/v2\/(account\/(myinfo|mine)|feed\/index)\? url script-request-header https://raw.githubusercontent.com/Fly961219/GG/refs/heads/main/ziyongbb.js
+^https?:\/\/app\.bilibili\.com\/bilibili\.app\.playerunite\.v1\.Player\/PlayViewUnite url script-request-header https://raw.githubusercontent.com/Fly961219/GG/refs/heads/main/ziyongbb.js
 
 [mitm]
 hostname = *.bilibili.*, *.biliapi.*, 111.31.33.*, 111.6.167.*, app.bilibili.com, api.bilibili.com, api.live.bilibili.com, grpc.biliapi.net
