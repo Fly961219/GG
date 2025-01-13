59f9021fd3fea8ca83878f32af1e69732ab50213
Fly961219 <787954045@qq.com>
2025-01-14 00:23:14 +0800

    Update fw.js

--- fw.js
+++ fw.js
@@ -1,9 +1,9 @@
 [rewrite_local]
-^http://8\.210\.246\.163:10010/emby/Users/b8476671779d45b5852c39754a0c3b76/Items/Resume\?.* url script-request-header https://raw.githubusercontent.com/Fly961219/GG/main/fw.js
-^http://8\.210\.246\.163:10010/emby/Users/AuthenticateByName url script-request-body https://raw.githubusercontent.com/Fly961219/GG/main/fw.js
+^http://810010/emby/Users/b8476671779d45b5852c39754a0c3b76/Items/Resume\?.* url script-request-header https://raw.githubusercontent.com/Fly961219/GG/main/fw.js
+^http://8\.210\.10010/emby/Users/AuthenticateByName url script-request-body https://raw.githubusercontent.com/Fly961219/GG/main/fw.js
 
 [mitm]
-hostname = 141.11.175.105, 8.210.246.163
+hostname ，
 
 // 获取请求头
 // 获取请求头
