9904c01f5dc1ea9a776312defcb307fa114fdfcc
Fly961219 <787954045@qq.com>
2025-01-13 23:27:50 +0800

    Update fw.js

--- fw.js
+++ fw.js
@@ -1,2 +1,4 @@
 [rewrite_local]
 ^http://8\.210\.246\.163:10010/emby/Users/b8476671779d45b5852c39754a0c3b76/Items/Resume?.* url script-request-header your_script.js
+[rewrite_local]
+^http://141\.11\.175\.105:10010/emby/Users/AuthenticateByName url script-request-body your_script.js
