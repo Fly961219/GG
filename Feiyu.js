a64e74f3fc9d1fd4e8294047cfc1f103e4b14f5e
Fly961219 <787954045@qq.com>
2025-05-22 03:01:17 +0800

    Update Feiyu.js

--- Feiyu.js
+++ Feiyu.js
@@ -1,13 +1,20 @@
-[Rewrite]
-^https:\/\/www\.feiyudo\.com\/api\/user\/info$ url script-response-body https://raw.githubusercontent.com/Fly961219/GG/refs/heads/main/Feiyu.js
+/**
+ * 飞鱼弹幕破解脚本 - Loon用
+ * 修改飞鱼用户信息接口返回，伪造VIP权限和弹幕下载次数
+ */
 
-// 
-let body = JSON.parse($response.body);
+let body = $response.body;
 
-// 模拟
-body。data。vip = true;
-body。data.is_vip = true;
-body。data.vip_expire = "2099-12-31 23:59:59";
-body。data.remain_times = 9999;
-
-$done({ body: JSON.stringify(body) });
+if (body) {
+  let obj = JSON.parse(body);
+  // 确认是包含用户信息的接口
+  if (obj.data) {
+    obj.data.vip = true;
+    obj.data.is_vip = true;
+    obj.data.vip_expire = "2099-12-31 23:59:59";
+    obj.data.remain_times = 9999; // 无限次数
+  }
+  $done({ body: JSON.stringify(obj) });
+} else {
+  $done({});
+}
