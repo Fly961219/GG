769940722458233f70401a17f1a41a041b0ec934
Fly961219 <787954045@qq.com>
2025-05-22 02:53:32 +0800

    Update Feiyu.js

--- Feiyu.js
+++ Feiyu.js
@@ -1,13 +1,13 @@
 [Rewrite]
-^https:\/\/www\.feiyudo\.com\/api\/user\/info$ url script-response-body feiyu.js
+^https:\/\/www\.feiyudo\.com\/api\/user\/info$ url script-response-body https://raw.githubusercontent.com/Fly961219/GG/refs/heads/main/Feiyu.js
 
 // 
 let body = JSON.parse($response.body);
 
 // 模拟
-body.data.vip = true;
-body.data.is_vip = true;
-body.data.vip_expire = "2099-12-31 23:59:59";
-body.data.remain_times = 9999;
+body。data。vip = true;
+body。data.is_vip = true;
+body。data.vip_expire = "2099-12-31 23:59:59";
+body。data.remain_times = 9999;
 
 $done({ body: JSON.stringify(body) });
